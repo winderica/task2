4dafdd7f94f18765a6ea1ca6b7fac2220e6e0edf
winderica <winderica@gmail.com>
2017-11-14 21:53:11 +0800

    6/10

--- script/index.js
+++ script/index.js
@@ -7,15 +7,18 @@ function toggle(i) {
             for (var j = 0; j < btn.length; j++) {
                 if (j !== i) {
                     if (flag[j]) {
+                        btn[j].style.backgroundColor = "transparent";
                         content[j].style.display = "none";
                         flag[j] = false;
                     }
                 }
             }
+            btn[i].style.backgroundColor = "rgb(26, 26, 26)";
             content[i].style.display = "block";
             flag[i] = true;
         }
         else {
+            btn[i].style.backgroundColor = "transparent";
             content[i].style.display = "none";
             flag[i] = false;
         }
@@ -27,6 +30,7 @@ for (var i = 0; i < btn.length; i++) {
 document.getElementsByClassName("block-1")[0].addEventListener("click", function () {
     for (var i = 0; i < btn.length; i++) {
         if (flag[i]) {
+            btn[i].style.backgroundColor = "transparent";
             content[i].style.display = "none";
             flag[i] = false;
         }
